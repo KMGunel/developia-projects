ea08423597ea03d562af5665cfd4241bb64bbab9
KMGunel <89314536+KMGunel@users.noreply.github.com>
2022-08-18 16:54:11 +0400

    unsucces

--- src/eolympTask/LazyVisitor.java
+++ src/eolympTask/LazyVisitor.java
@@ -1,8 +1,46 @@
 package eolympTask;
 
+import java.util.Scanner;
+
 public class LazyVisitor {
 	public static void main(String[] args) {
 		
-	}
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int[][]arr= new int[n][2];
+		
+		for(int i = 0;i<arr.length;i++) {				
+				arr[i][0]= sc.nextInt();
+				arr[i][1]= sc.nextInt();			
+		}
+		
+		int count=0;		
+		for(int i = 0;i<arr.length;i++) {	
+			for(int j = arr[i][0];j<=arr[i][1];j++) {
+				if(i<arr.length-1) {
+					for(int k = arr[i+1][0];k<=arr[i+1][1];k++) {
+						if(j==k) {
+							System.out.println(i);					
+							count++;
+							break;
+						}					
+						
+					}
+					
+					break;						
+				}
+			}
+		}
+		
+		if(count ==0 ) {
+		System.out.println("Result = " + n);
+		}
+		else {
+			System.out.println("Result = " + count);
+		}
+		
+		
+	}		
+}				
 
-}
