ace022c2731cbd2c1d3a6521712f16991e026084
KMGunel <89314536+KMGunel@users.noreply.github.com>
2022-08-30 21:41:48 +0400

    array tasks has done

--- src/w3resourceTask/arraysTask/Task12.java
+++ src/w3resourceTask/arraysTask/Task12.java
@@ -3,20 +3,15 @@ package w3resourceTask.arraysTask;
 public class Task12 {
 
 	public static void main(String[] args) {
-		int[] arr = {7,4,6,2,4,8,4,6,9,11};
+		int[] arr = {2,4,6,2,4,85,11,45,6,99};
 		
-		for (int i = 0,j=1; i < arr.length; i++) {
-			
-				if(arr[i]==arr[j]) {
-				System.out.println(arr[i]);
-				
-				}
-				j++;
-				if(j==arr.length-1) {
-					break;
-				}
-												
-		
-		}
+		for (int i = 0; i < arr.length-1; i++) {
+			for (int j = i+1; j < arr.length; j++) {
+				if(arr[i]==arr[j] && i!=j) {
+					System.out.println(arr[j]);
+					break;		
+			}			
+		}			
+		}		
 	}
 }
